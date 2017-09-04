d760e043dfaa62d801728550a8e492ec04bc485e
Guto Maia <guto@guto.net>
2017-09-04 14:19:30 -0300

    added a default VERSION.txt on github.mk

--- github.mk
+++ github.mk
@@ -2,13 +2,16 @@
 PROJECT_NAME?=Project
 PROJECT_TAG?=project
 
+VERSION.txt:
+	echo '0.0.0' > VERSION.txt
+
 before-tag:
 	@rm -f /tmp/.${PROJECT_TAG}_commitMSG | echo "OK"
 	@git branch | grep -P '\* \d+\.\d+\.x' || (echo "You must be in a version branch" && exit 1)
 	@#TODO: you must be on a branch that matches the version
 	@#TODO: you must be synced with the remote branch
 
-patch: test before-tag
+patch: test before-tag VERSION.txt
 	@python -c "f = open('VERSION.txt'); a = f.read().strip('\r\n').split('.'); f.close(); c = [int(b) for b in a]; c[2] += 1; print '.'.join([str(d) for d in c])" > tmp; mv tmp VERSION.txt
 	@echo "patch" > /tmp/.${PROJECT_TAG}_commitMSG
 	@make create-tag
