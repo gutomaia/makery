370e03d5d04d7921bdf3cd1dba38f0a2c066061e
Guto Maia <guto@guto.net>
2024-03-30 21:00:30 -0300

    fixes github tasks for patch, minor and major

--- github.mk
+++ github.mk
@@ -8,53 +8,58 @@ PROJECT_TAG?=project
 CHECKPOINT_DIR?=.checkpoint
 REQUIREMENTS_TEST?=${CHECKPOINT_DIR}/requirements_test.txt
 VERSION_FILE?=${PYTHON_MODULES}/__init__.py
-CURRENT_VERSION?=$(shell python setup.py --version)
+CURRENT_VERSION?=$(shell poetry version | cut -d' ' -f 2)
 CHANGELOG_FILE?=CHANGELOG.md
 CHANGELOG_DOC?=docs/changelog.rst
 GIT_TAG_EXTRA_ARGS?=
 DEFAULT_VERSION_BEHAVIOR=test
+REQUIREMENTS_LICENCES=docs/requirements_licenses.rst
+REQUIREMENTS_DEV_LICENSES=docs/requirements_dev_licenses.rst
 
 major: ${DEFAULT_VERSION_BEHAVIOR} ${REQUIREMENTS_TEST}
 	@echo "Bump major version file (${VERSION_FILE}): \c"
-	${VIRTUALENV} bumpversion --current-version ${CURRENT_VERSION} major ${VERSION_FILE}
+	${VIRTUALENV} poetry version major
 	${CHECK}
 	@make create-tag
 
 minor: ${DEFAULT_VERSION_BEHAVIOR} ${REQUIREMENTS_TEST}
 	@echo "Bump minor version file (${VERSION_FILE}): \c"
-	${VIRTUALENV} bumpversion --current-version ${CURRENT_VERSION} minor ${VERSION_FILE}
+	${VIRTUALENV} poetry version minor
 	${CHECK}
 	@make create-tag
 
 patch: ${DEFAULT_VERSION_BEHAVIOR} ${REQUIREMENTS_TEST}
 	@echo "Bump patch version file (${VERSION_FILE}): \c"
-	${VIRTUALENV} bumpversion --current-version ${CURRENT_VERSION} patch ${VERSION_FILE}
+	${VIRTUALENV} poetry version patch
 	${CHECK}
 	@make create-tag
 
-create-tag:
+create-tag: ${REQUIREMENTS_LICENCES} ${REQUIREMENTS_DEV_LICENSES}
 	@echo "Added version file (${VERSION_FILE}) to repository: \c"
 	@git add ${VERSION_FILE}
+	@git add pyproject.toml
+	@git add ${REQUIREMENTS_LICENCES}
+	@git add ${REQUIREMENTS_DEV_LICENSES}
 	${CHECK}
 
-	@python setup.py --version | xargs -I [] echo "Commit version v"[]" to repository: \c"
-	@python setup.py --version | xargs -I [] git commit -m "New ${PROJECT_NAME} version []" -q
+	${VIRTUALENV} poetry version | cut -d' ' -f 2 | xargs -I [] echo "Commit version v"[]" to repository: \c"
+	${VIRTUALENV} poetry version | cut -d' ' -f 2 | xargs -I [] git commit -m "New ${PROJECT_NAME} version []" -q
 	${CHECK}
 
-	@python setup.py --version | xargs -I [] echo "Temporary tag version v"[]" to repository: \c"
-	@python setup.py --version | xargs -I [] git tag -a v[] -m '${PROJECT_NAME} version []' ${GIT_TAG_EXTRA_ARGS}
+	${VIRTUALENV} poetry version | cut -d' ' -f 2 | xargs -I [] echo "Temporary tag version v"[]" to repository: \c"
+	${VIRTUALENV} poetry version | cut -d' ' -f 2 | xargs -I [] git tag -a v[] -m '${PROJECT_NAME} version []' ${GIT_TAG_EXTRA_ARGS}
 	${CHECK}
 
 	@echo "Create changelog file (${CHANGELOG_FILE}): \c"
-	${VIRTUALENV} git-changelog -s angular . -o ${CHANGELOG_FILE}
+	${VIRTUALENV} git-changelog -c angular . -o ${CHANGELOG_FILE}
 	${CHECK}
 
 	@echo "Create changelog doc file (${CHANGELOG_DOC}): \c"
 	${VIRTUALENV} m2r ${CHANGELOG_FILE} && mv CHANGELOG.rst ${CHANGELOG_DOC}
 	${CHECK}
 
-	@python setup.py --version | xargs -I [] echo "Remove temporary tag version v"[]" from repository: \c"
-	@python setup.py --version | xargs -I [] git tag -d v[]
+	${VIRTUALENV} poetry version | cut -d' ' -f 2 | xargs -I [] echo "Remove temporary tag version v"[]" from repository: \c"
+	${VIRTUALENV} poetry version | cut -d' ' -f 2 | xargs -I [] git tag -d v[]
 	${CHECK}
 
 	@echo "Added changelog file (${CHANGELOG_FILE}) to repository: \c"
@@ -72,7 +77,7 @@ create-tag:
 	${CHECK}
 
 	@echo "Create changelog file (${VERSION_FILE}): \c"
-	@python setup.py --version | xargs -I [] git tag -a v[] -m '${PROJECT_NAME} version []' ${GIT_TAG_EXTRA_ARGS}
+	${VIRTUALENV} poetry version | cut -d' ' -f 2 | xargs -I [] git tag -a v[] -m '${PROJECT_NAME} version []' ${GIT_TAG_EXTRA_ARGS}
 	${CHECK}
 
 	@echo "Push branch to repository: \c"
